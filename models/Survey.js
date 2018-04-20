808d33dbc15eddd6e4479f79b2fc63e14c6abd79
emmanuel villegas <evillegas@condorlabs.io>
2018-04-20 09:56:59 -0500

    modify survey schema

--- models/Survey.js
+++ models/Survey.js
@@ -6,7 +6,9 @@ const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String]
+  recipients: [String],
+  yes: { type: Number, default: 0 },
+  No: { type: Number, default: 0 }
 });
 
 mongoose.model('surveys', surveySchema);
